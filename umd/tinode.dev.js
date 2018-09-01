ce3c4db2f9deac0a19ee01e1c1641a0d5a7066ec
or-else <or.else@gmail.com>
2018-09-01 14:21:45 +0300

    update generated files

--- umd/tinode.dev.js
+++ umd/tinode.dev.js
@@ -1162,7 +1162,6 @@ function jsonLoggerHelper(key, val) {
 
 // Parse browser user agent to extract browser name and version.
 function getBrowserInfo(ua) {
-
   // First test for WebKit based browser.
   ua = (ua||'').replace(' (KHTML, like Gecko)', '');
   var m = ua.match(/(AppleWebKit\/[.\d]+)/i);
