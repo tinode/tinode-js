8eba95ab867e763559a9f5065ab234fd9d4dd634
or-else <or.else@gmail.com>
2018-11-13 10:11:05 +0300

    check entity for validity before use

--- src/drafty.js
+++ src/drafty.js
@@ -1201,7 +1201,7 @@ Drafty.isPlainText = function(content) {
 Drafty.hasAttachments = function(content) {
   if (content.ent && content.ent.length > 0) {
     for (var i in content.ent) {
-      if (content.ent[i].tp == 'EX') {
+      if (content.ent[i] && content.ent[i].tp == 'EX') {
         return true;
       }
     }
@@ -1232,7 +1232,7 @@ Drafty.hasAttachments = function(content) {
 Drafty.attachments = function(content, callback, context) {
   if (content.ent && content.ent.length > 0) {
     for (var i in content.ent) {
-      if (content.ent[i].tp == 'EX') {
+      if (content.ent[i] && content.ent[i].tp == 'EX') {
         callback.call(context, content.ent[i].data, i);
       }
     }
