0cf822749de86c160cc06a56ac939426204aee1f
or-else <or.else@gmail.com>
2021-06-09 19:52:02 -0700

    split MetaGetBuilder into separate file, fix cache timestamps

---
+++ src/meta-builder.js
@@ -0,0 +1,261 @@
+/**
+ * @file Helper class for constructing {@link Tinode.GetQuery}.
+ *
+ * @copyright 2015-2021 Tinode
+ * @summary Javascript bindings for Tinode.
+ * @license Apache 2.0
+ * @version 0.17
+ */
+'use strict';
+
+/**
+ * Helper class for constructing {@link Tinode.GetQuery}.
+ *
+ * @class MetaGetBuilder
+ * @memberof Tinode
+ *
+ * @param {Tinode.Topic} parent topic which instantiated this builder.
+ */
+const MetaGetBuilder = function(parent) {
+  this.topic = parent;
+  this.what = {};
+}
+
+MetaGetBuilder.prototype = {
+
+  // Get timestamp of the most recent desc update.
+  _get_desc_ims: function() {
+    return this.topic.updated;
+  },
+
+  // Get timestamp of the most recent subs update.
+  _get_subs_ims: function() {
+    if (this.topic.isP2PType()) {
+      return this._get_desc_ims();
+    }
+    if (this.topic.isMeType()) {
+      const gupd = this.topic._tinode._lastTopicUpdate || 0;
+      if (gupd > this.topic._lastSubsUpdate) {
+        return gupd;
+      }
+    }
+    return this.topic._lastSubsUpdate;
+  },
+
+  /**
+   * Add query parameters to fetch messages within explicit limits.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {number=} since - messages newer than this (inclusive);
+   * @param {number=} before - older than this (exclusive)
+   * @param {number=} limit - number of messages to fetch
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withData: function(since, before, limit) {
+    this.what['data'] = {
+      since: since,
+      before: before,
+      limit: limit
+    };
+    return this;
+  },
+
+  /**
+   * Add query parameters to fetch messages newer than the latest saved message.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {number=} limit - number of messages to fetch
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withLaterData: function(limit) {
+    return this.withData(this.topic._maxSeq > 0 ? this.topic._maxSeq + 1 : undefined, undefined, limit);
+  },
+
+  /**
+   * Add query parameters to fetch messages older than the earliest saved message.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {number=} limit - maximum number of messages to fetch.
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withEarlierData: function(limit) {
+    return this.withData(undefined, this.topic._minSeq > 0 ? this.topic._minSeq : undefined, limit);
+  },
+
+  /**
+   * Add query parameters to fetch topic description if it's newer than the given timestamp.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {Date=} ims - fetch messages newer than this timestamp.
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withDesc: function(ims) {
+    this.what['desc'] = {
+      ims: ims
+    };
+    return this;
+  },
+
+  /**
+   * Add query parameters to fetch topic description if it's newer than the last update.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withLaterDesc: function() {
+    return this.withDesc(this._get_desc_ims());
+  },
+
+  /**
+   * Add query parameters to fetch subscriptions.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {Date=} ims - fetch subscriptions modified more recently than this timestamp
+   * @param {number=} limit - maximum number of subscriptions to fetch.
+   * @param {string=} userOrTopic - user ID or topic name to fetch for fetching one subscription.
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withSub: function(ims, limit, userOrTopic) {
+    const opts = {
+      ims: ims,
+      limit: limit
+    };
+    if (this.topic.getType() == 'me') {
+      opts.topic = userOrTopic;
+    } else {
+      opts.user = userOrTopic;
+    }
+    this.what['sub'] = opts;
+    return this;
+  },
+
+  /**
+   * Add query parameters to fetch a single subscription.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {Date=} ims - fetch subscriptions modified more recently than this timestamp
+   * @param {string=} userOrTopic - user ID or topic name to fetch for fetching one subscription.
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withOneSub: function(ims, userOrTopic) {
+    return this.withSub(ims, undefined, userOrTopic);
+  },
+
+  /**
+   * Add query parameters to fetch a single subscription if it's been updated since the last update.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {string=} userOrTopic - user ID or topic name to fetch for fetching one subscription.
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withLaterOneSub: function(userOrTopic) {
+    return this.withOneSub(this.topic._lastSubsUpdate, userOrTopic);
+  },
+
+  /**
+   * Add query parameters to fetch subscriptions updated since the last update.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {number=} limit - maximum number of subscriptions to fetch.
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withLaterSub: function(limit) {
+    return this.withSub(this._get_subs_ims(), limit);
+  },
+
+  /**
+   * Add query parameters to fetch topic tags.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withTags: function() {
+    this.what['tags'] = true;
+    return this;
+  },
+
+  /**
+   * Add query parameters to fetch user's credentials. <code>'me'</code> topic only.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withCred: function() {
+    if (this.topic.getType() == 'me') {
+      this.what['cred'] = true;
+    } else {
+      this.topic._tinode.logger("ERROR: Invalid topic type for MetaGetBuilder:withCreds", this.topic.getType());
+    }
+    return this;
+  },
+
+  /**
+   * Add query parameters to fetch deleted messages within explicit limits. Any/all parameters can be null.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {number=} since - ids of messages deleted since this 'del' id (inclusive)
+   * @param {number=} limit - number of deleted message ids to fetch
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withDel: function(since, limit) {
+    if (since || limit) {
+      this.what['del'] = {
+        since: since,
+        limit: limit
+      };
+    }
+    return this;
+  },
+
+  /**
+   * Add query parameters to fetch messages deleted after the saved <code>'del'</code> id.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @param {number=} limit - number of deleted message ids to fetch
+   *
+   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
+   */
+  withLaterDel: function(limit) {
+    // Specify 'since' only if we have already received some messages. If
+    // we have no locally cached messages then we don't care if any messages were deleted.
+    return this.withDel(this.topic._maxSeq > 0 ? this.topic._maxDel + 1 : undefined, limit);
+  },
+
+  /**
+   * Construct parameters.
+   * @memberof Tinode.MetaGetBuilder#
+   *
+   * @returns {Tinode.GetQuery} Get query
+   */
+  build: function() {
+    const what = [];
+    const instance = this;
+    let params = {};
+    ['data', 'sub', 'desc', 'tags', 'cred', 'del'].map(function(key) {
+      if (instance.what.hasOwnProperty(key)) {
+        what.push(key);
+        if (Object.getOwnPropertyNames(instance.what[key]).length > 0) {
+          params[key] = instance.what[key];
+        }
+      }
+    });
+    if (what.length > 0) {
+      params.what = what.join(' ');
+    } else {
+      params = undefined;
+    }
+    return params;
+  }
+};
+
+if (typeof module != 'undefined') {
+  module.exports = MetaGetBuilder;
+}
--- src/tinode.js
+++ src/tinode.js
@@ -56,6 +56,7 @@ const Connection = require('./connection.js');
 const DBCache = require('./db.js');
 const Drafty = require('./drafty.js');
 const LargeFileHelper = require('./large-file.js');
+const MetaGetBuilder = require('./meta-builder.js');
 
 const package_version = require('../version.json').version;
 
@@ -2315,255 +2316,6 @@ Tinode.prototype = {
 };
 
 /**
- * Helper class for constructing {@link Tinode.GetQuery}.
- *
- * @class MetaGetBuilder
- * @memberof Tinode
- *
- * @param {Tinode.Topic} parent topic which instantiated this builder.
- */
-var MetaGetBuilder = function(parent) {
-  this.topic = parent;
-  this.what = {};
-}
-
-MetaGetBuilder.prototype = {
-
-  // Get timestamp of the most recent desc update.
-  _get_desc_ims: function() {
-    const tupd = this.topic._lastDescUpdate || 0;
-    return this.topic.updated > tupd ? this.topic.updated : tupd;
-  },
-
-  // Get timestamp of the most recent subs update.
-  _get_subs_ims: function() {
-    if (this.topic.isP2PType()) {
-      return this._get_desc_ims();
-    }
-    if (this.topic.isMeType()) {
-      const gupd = this.topic._tinode._lastTopicUpdate || 0;
-      if (gupd > this.topic._lastSubsUpdate) {
-        return gupd;
-      }
-    }
-    return this.topic._lastSubsUpdate;
-  },
-
-  /**
-   * Add query parameters to fetch messages within explicit limits.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {number=} since - messages newer than this (inclusive);
-   * @param {number=} before - older than this (exclusive)
-   * @param {number=} limit - number of messages to fetch
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withData: function(since, before, limit) {
-    this.what['data'] = {
-      since: since,
-      before: before,
-      limit: limit
-    };
-    return this;
-  },
-
-  /**
-   * Add query parameters to fetch messages newer than the latest saved message.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {number=} limit - number of messages to fetch
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withLaterData: function(limit) {
-    return this.withData(this.topic._maxSeq > 0 ? this.topic._maxSeq + 1 : undefined, undefined, limit);
-  },
-
-  /**
-   * Add query parameters to fetch messages older than the earliest saved message.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {number=} limit - maximum number of messages to fetch.
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withEarlierData: function(limit) {
-    return this.withData(undefined, this.topic._minSeq > 0 ? this.topic._minSeq : undefined, limit);
-  },
-
-  /**
-   * Add query parameters to fetch topic description if it's newer than the given timestamp.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {Date=} ims - fetch messages newer than this timestamp.
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withDesc: function(ims) {
-    this.what['desc'] = {
-      ims: ims
-    };
-    return this;
-  },
-
-  /**
-   * Add query parameters to fetch topic description if it's newer than the last update.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withLaterDesc: function() {
-    return this.withDesc(this._get_desc_ims());
-  },
-
-  /**
-   * Add query parameters to fetch subscriptions.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {Date=} ims - fetch subscriptions modified more recently than this timestamp
-   * @param {number=} limit - maximum number of subscriptions to fetch.
-   * @param {string=} userOrTopic - user ID or topic name to fetch for fetching one subscription.
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withSub: function(ims, limit, userOrTopic) {
-    const opts = {
-      ims: ims,
-      limit: limit
-    };
-    if (this.topic.getType() == 'me') {
-      opts.topic = userOrTopic;
-    } else {
-      opts.user = userOrTopic;
-    }
-    this.what['sub'] = opts;
-    return this;
-  },
-
-  /**
-   * Add query parameters to fetch a single subscription.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {Date=} ims - fetch subscriptions modified more recently than this timestamp
-   * @param {string=} userOrTopic - user ID or topic name to fetch for fetching one subscription.
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withOneSub: function(ims, userOrTopic) {
-    return this.withSub(ims, undefined, userOrTopic);
-  },
-
-  /**
-   * Add query parameters to fetch a single subscription if it's been updated since the last update.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {string=} userOrTopic - user ID or topic name to fetch for fetching one subscription.
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withLaterOneSub: function(userOrTopic) {
-    return this.withOneSub(this.topic._lastSubsUpdate, userOrTopic);
-  },
-
-  /**
-   * Add query parameters to fetch subscriptions updated since the last update.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {number=} limit - maximum number of subscriptions to fetch.
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withLaterSub: function(limit) {
-    return this.withSub(this._get_subs_ims(), limit);
-  },
-
-  /**
-   * Add query parameters to fetch topic tags.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withTags: function() {
-    this.what['tags'] = true;
-    return this;
-  },
-
-  /**
-   * Add query parameters to fetch user's credentials. <code>'me'</code> topic only.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withCred: function() {
-    if (this.topic.getType() == 'me') {
-      this.what['cred'] = true;
-    } else {
-      this.topic._tinode.logger("ERROR: Invalid topic type for MetaGetBuilder:withCreds", this.topic.getType());
-    }
-    return this;
-  },
-
-  /**
-   * Add query parameters to fetch deleted messages within explicit limits. Any/all parameters can be null.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {number=} since - ids of messages deleted since this 'del' id (inclusive)
-   * @param {number=} limit - number of deleted message ids to fetch
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withDel: function(since, limit) {
-    if (since || limit) {
-      this.what['del'] = {
-        since: since,
-        limit: limit
-      };
-    }
-    return this;
-  },
-
-  /**
-   * Add query parameters to fetch messages deleted after the saved <code>'del'</code> id.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @param {number=} limit - number of deleted message ids to fetch
-   *
-   * @returns {Tinode.MetaGetBuilder} <code>this</code> object.
-   */
-  withLaterDel: function(limit) {
-    // Specify 'since' only if we have already received some messages. If
-    // we have no locally cached messages then we don't care if any messages were deleted.
-    return this.withDel(this.topic._maxSeq > 0 ? this.topic._maxDel + 1 : undefined, limit);
-  },
-
-  /**
-   * Construct parameters.
-   * @memberof Tinode.MetaGetBuilder#
-   *
-   * @returns {Tinode.GetQuery} Get query
-   */
-  build: function() {
-    const what = [];
-    const instance = this;
-    let params = {};
-    ['data', 'sub', 'desc', 'tags', 'cred', 'del'].map(function(key) {
-      if (instance.what.hasOwnProperty(key)) {
-        what.push(key);
-        if (Object.getOwnPropertyNames(instance.what[key]).length > 0) {
-          params[key] = instance.what[key];
-        }
-      }
-    });
-    if (what.length > 0) {
-      params.what = what.join(' ');
-    } else {
-      params = undefined;
-    }
-    return params;
-  }
-};
-
-/**
  * @callback Tinode.Topic.onData
  * @param {Data} data - Data packet
  */
@@ -2591,13 +2343,13 @@ var Topic = function(name, callbacks) {
   // Server-provided data, locally immutable.
   // topic name
   this.name = name;
-  // timestamp when the topic was created
+  // Timestamp when the topic was created.
   this.created = null;
-  // timestamp when the topic was last updated
+  // Timestamp when the topic was last updated.
   this.updated = null;
-  // timestamp of the last messages
-  this.touched = null;
-  // access mode, see AccessMode
+  // Timestamp of the last messages
+  this.touched = new Date(0);
+  // Access mode, see AccessMode
   this.acs = new AccessMode(null);
   // per-topic private data
   this.private = null;
@@ -2629,10 +2381,8 @@ var Topic = function(name, callbacks) {
   }, true);
   // Boolean, true if the topic is currently live
   this._subscribed = false;
-  // Timestap when topic meta-desc update was recived.
-  this._lastDescUpdate = null;
-  // Timestap when topic meta-subs update was recived.
-  this._lastSubsUpdate = null;
+  // Timestap of the most recently updated subscription.
+  this._lastSubsUpdate = new Date(0);
   // Topic created but not yet synced with the server. Used only during initialization.
   this._new = true;
 
@@ -3796,11 +3546,9 @@ Topic.prototype = {
   // Process metadata message
   _routeMeta: function(meta) {
     if (meta.desc) {
-      this._lastDescUpdate = meta.ts;
       this._processMetaDesc(meta.desc);
     }
     if (meta.sub && meta.sub.length > 0) {
-      this._lastSubsUpdate = meta.ts;
       this._processMetaSub(meta.sub);
     }
     if (meta.del) {
@@ -3952,6 +3700,8 @@ Topic.prototype = {
       sub.updated = new Date(sub.updated);
       sub.deleted = sub.deleted ? new Date(sub.deleted) : null;
       sub.online = !!sub.online;
+      // Update timestamp of the most recent subscription update.
+      this._lastSubsUpdate = new Date(Math.max(this._lastSubsUpdate, sub.updated));
 
       let user = null;
       if (!sub.deleted) {
@@ -3959,7 +3709,7 @@ Topic.prototype = {
         // Desc will update 'me' topic.
         if (this._tinode.isMe(sub.user) && sub.acs) {
           this._processMetaDesc({
-            updated: sub.updated || new Date(),
+            updated: sub.updated,
             touched: sub.updated,
             acs: sub.acs
           });
